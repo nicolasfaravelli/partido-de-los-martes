7d645c8f00d77d3fb6cafe79f7af909de832718a
nicolasfaravelli <141712279+nicolasfaravelli@users.noreply.github.com>
2026-06-11 13:31:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -112,8 +112,8 @@ function renderizarModal(j) {
            if(res === 'P') colorRes = '#F44336'; 
            return `
               <div style="display:flex; flex-direction:column; align-items:center; width:16%;">
-              <span style="font-family:var(--fuente-impacto); font-size:9cqw; color:${j.color}; line-height: 1;">${fecha}</span>   
-              <span style="font-family:var(--fuente-impacto); font-size:9cqw; color:${colorRes}; line-height: 1; margin-top: -1cqw;">${res}</span>
+              <span style="font-family:var(--fuente-impacto); font-size:8.5cqw; color:${j.color}; line-height: 1;">${fecha}</span>   
+              <span style="font-family:var(--fuente-impacto); font-size:8.5cqw; color:${colorRes}; line-height: 1; margin-top: -0.75cqw;">${res}</span>
         </div>
             `;
         }).join('');
@@ -133,7 +133,7 @@ function renderizarModal(j) {
                         </div>
                         <div class="info-layer" style="color:${j.color}">
                             <div class="name">${j.nombre}</div>
-                            <div class="stats-container" style="top: 75%; left: 5%; width: 90%; justify-content: space-between;">
+                            <div class="stats-container" style="top: 76%; left: 5%; width: 90%; justify-content: space-between;">
                                 ${htmlRacha}
                             </div>
                         </div>
