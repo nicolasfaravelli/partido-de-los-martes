1216493665c0a4fc9a89b3544e3f7ed1b3661b15
nicolasfaravelli <141712279+nicolasfaravelli@users.noreply.github.com>
2026-06-12 05:38:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -113,10 +113,10 @@ function renderizarModal(j) {
         const espaciadoLetraFecha = "-0.3px";   
         const espacioHaciaCuadrado = "0px";  // El espacio vacío EXACTO entre las fechas y los cuadrados
         // --- 3. LOS CUADRADOS ---
-        const anchoCuadrado = "60%";         
+        const anchoCuadrado = "55%";         
         const redondeoCuadrado = "100px";      
         // --- 4. LAS LETRAS DE RESULTADO (EL DESFASE VISUAL) ---
-        const tamLetraRes = "6.5cqw";        
+        const tamLetraRes = "5.5cqw";        
         const microAjusteLetraV = "1.2px";      
         const htmlRacha = ultimasFechas.map((fecha, i) => {
         const res = (j.racha[i] || "-").trim().toUpperCase();            
