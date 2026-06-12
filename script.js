c48006f3c53c45c5f4bc2046748b094576d41b14
nicolasfaravelli <141712279+nicolasfaravelli@users.noreply.github.com>
2026-06-12 05:19:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -109,13 +109,13 @@ function renderizarModal(j) {
         const posVBloque = "76%";            // Bajalo o subilo para alejarlo/acercarlo a la línea del nombre
          // --- 2. LAS FECHAS ---
         const tamFuenteFecha = "8.75cqw";    
-        const grosorFecha = "575";          
+        const grosorFecha = "585";          
         const anchoEscalaFecha = "0.9";        
         const espaciadoLetraFecha = "-0.25px";   
         const espacioHaciaCuadrado = "1px";  // El espacio vacío EXACTO entre las fechas y los cuadrados
         // --- 3. LOS CUADRADOS ---
         const anchoCuadrado = "50%";         
-        const redondeoCuadrado = "4px";      
+        const redondeoCuadrado = "100px";      
         
         // --- 4. LAS LETRAS DE RESULTADO (EL DESFASE VISUAL) ---
         const tamLetraRes = "6.5cqw";        
